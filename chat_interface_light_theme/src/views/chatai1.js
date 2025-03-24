53aff181c2733229e5e663617413c5c2ddc599e1
AtharvaChaudhari07 <atharvaschaudhari07@gmail.com>
2025-03-24 23:02:25 +0530

    Add files via upload

---
+++ chat_interface_light_theme/src/views/chatai1.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './chatai1.css'
+
+const Chatai1 = (props) => {
+  return (
+    <div className="chatai1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="chatai1-dummy-container">
+        <img
+          src="/external/chatai1113-o6tl-1600w.png"
+          alt="chatai1113"
+          className="chatai1-chatai1"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Chatai1
